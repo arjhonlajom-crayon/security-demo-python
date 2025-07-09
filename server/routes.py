01870884ed29193fd02996503417166f43850696
arjhonlajom-crayon <arjhon.lajom@crayon.com>
2025-07-09 13:18:37 +0800

    Update routes.py
    updated SQL scripts to include semicolon

--- server/routes.py
+++ server/routes.py
@@ -13,16 +13,16 @@ def index():
 
     if name:
         
-        cursor.execute("SELECT * FROM books WHERE name LIKE %s", name)
+        cursor.execute("SELECT * FROM books WHERE name LIKE %s;", name)
         books = [Book(*row) for row in cursor]
 
     elif author:
         
-        cursor.execute("SELECT * FROM books WHERE author LIKE %s", author)
+        cursor.execute("SELECT * FROM books WHERE author LIKE %s;", author)
         books = [Book(*row) for row in cursor]
 
     else:
-        cursor.execute("SELECT name, author, read FROM books")
+        cursor.execute("SELECT name, author, read FROM books;")
         books = [Book(*row) for row in cursor]
 
     return render_template('books.html', books=books)
