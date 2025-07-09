edc3d8eb9929b275fb7d1a26d236f1d4f62c9d3c
arjhonlajom-crayon <arjhon.lajom@crayon.com>
2025-07-09 13:46:28 +0800

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -17,7 +17,6 @@ def index():
         books = [Book(*row) for row in cursor]
 
     elif author:
-        
         cursor.execute("SELECT * FROM books WHERE author LIKE %s;", author)
         books = [Book(*row) for row in cursor]
 
